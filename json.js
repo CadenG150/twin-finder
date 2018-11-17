4ac4ece60ddfcb954e13b8ce54dce71ef3862304
CadenG150 <cadengrey@icloud.com>
2018-11-17 16:12:30 -0500

    Finished project

---
+++ json.js
@@ -0,0 +1,75 @@
+// JSON age variable
+
+var ages = [{
+    "name" : "Caden Grey",
+    "month" : "december",
+    "day" : "7",
+    "search_string" : "december 7"
+},
+{
+    "name" : "Skyler Cole",
+    "month" : "may",
+    "day" : "4",
+    "search_string" : "may 4"
+},
+{
+    "name" : "Aaron Carter",
+    "month" : "december",
+    "day" : "7",
+    "search_string" : "december 7"
+},
+{
+    "name" : "Ainsley Grey",
+    "month" : "february",
+    "day" : "5",
+    "search_string" : "february 5"
+},
+{
+    "name" : "Charlie Puth",
+    "month" : "december",
+    "day" : "2",
+    "search_string" : "december 2"
+},
+{
+    "name" : "Beyonce",
+    "month" : "september",
+    "day" : "4",
+    "search_string" : "september 4"
+},
+{
+    "name" : "Zac Efron",
+    "month" : "october",
+    "day" : "18",
+    "search_string" : "october 18"
+},
+{
+    "name" : "Cardi B",
+    "month" : "october",
+    "day" : "11",
+    "search_string" : "october 11"
+},
+{
+    "name" : "Landry Grey",
+    "month" : "july",
+    "day" : "7",
+    "search_string" : "july 7"
+},
+{
+    "name" : "Seth Davis",
+    "month" : "may",
+    "day" : "20",
+    "search_string" : "may 20"
+},
+{
+    "name" : "Ashelyn Grey",
+    "month" : "february",
+    "day" : "12",
+    "search_string" : "february 12"
+},
+{
+    "name" : "Kathy Readinger",
+    "month" : "october",
+    "day" : "20",
+    "search_string" : "october 20"
+}
+];
--- main.js
+++ main.js
@@ -1,3 +1,73 @@
+function stringSearch(searching_string) {
+  var b = 0;
+  for (let x = 0; x < ages.length; x++) {
+    var y = ages[x].search_string;
+    if (y == searching_string) {
+      console.log("Search String Match Found!");
+      var b = b + 1;
+      console.log(b)
+      document.getElementById("twins-text-" + b).innerHTML = ages[x].name + "\n";
+      console.log(ages[x].name);
+    } else {
+      console.log("Nothing Found")
+    }
+  }
+}
+
+function visibility() {
+  document.getElementById("input").style.visibility = "hidden";
+  document.getElementById("actual-button").style.visibility = "hidden";
+  document.getElementById("header").style.visibility = "hidden";
+  document.getElementById("about").style.visibility = "hidden";
+  document.getElementById("container").style.visibility = "hidden";
+  document.getElementById("header-2-text").style.visibility = "visible";
+  document.getElementById("underline-text").style.visibility = "visible";
+  document.getElementById("twins-text-1").style.visibility = "visible";
+  document.getElementById("twins-text-2").style.visibility = "visible";
+}
+
 function findTwin() {
-  alert("Twin Found")
+  var name = document.getElementById("input-box").value;
+  var month = document.getElementById("select-id").value;
+  var day = document.getElementById("select-id-2").value;
+  visibility()
+  if (month == "1") {
+    var simple_string = "january " + day;
+    stringSearch(simple_string)
+  } else if (month == "2") {
+    var simple_string = "february " + day;
+    stringSearch(simple_string)
+  } else if (month == "3") {
+    var simple_string = "march " + day;
+    stringSearch(simple_string)
+  } else if (month == "4") {
+    var simple_string = "april " + day;
+    stringSearch(simple_string)
+  } else if (month == "5") {
+    var simple_string = "may " + day;
+    stringSearch(simple_string)
+  } else if (month == "6") {
+    var simple_string = "june " + day;
+    stringSearch(simple_string)
+  } else if (month == "7") {
+    var simple_string = "july " + day;
+    stringSearch(simple_string)
+  } else if (month == "8") {
+    var simple_string = "august " + day;
+    stringSearch(simple_string)
+  } else if (month == "9") {
+    var simple_string = "september " + day;
+    stringSearch(simple_string)
+  } else if (month == "10") {
+    var simple_string = "october " + day;
+    stringSearch(simple_string)
+  } else if (month == "11") {
+    var simple_string = "november " + day;
+    stringSearch(simple_string)
+  } else if (month == "12") {
+    var simple_string = "december " + day;
+    stringSearch(simple_string)
+  } else {
+    console.log("Error")
+  }
 }
